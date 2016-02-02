e51dcbf1d9942e8bd0c1e066ab80dd24394e1df0
falexo <ao_2000@pisem.net>
2016-02-02 10:16:34 +0200

    Base project structure Created MainActivity & layout

---
+++ app/src/main/java/com/falexo/reminder/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.falexo.reminder.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by newman on 02.02.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
